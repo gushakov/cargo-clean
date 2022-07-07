c3d85cdbda309822a111af142b94fc99b0ead880
gushakov <Georgy.Ushakov@unil.ch>
2022-07-07 15:37:07 +0200

    legs_cargo_fk: cascade on delete

--- src/main/resources/db/migration/V1.2__Add_itinerary.sql
+++ src/main/resources/db/migration/V1.2__Add_itinerary.sql
@@ -6,7 +6,7 @@ CREATE TABLE public.leg
     load_time         timestamp NOT NULL,
     unload_time       timestamp NOT NULL,
     leg_index         int       NOT NULL,
+    CONSTRAINT leg_cargo_fk FOREIGN KEY (cargo_tracking_id) REFERENCES public.cargo (tracking_id) ON DELETE CASCADE,
     CONSTRAINT leg_load_location_fk FOREIGN KEY (load_location) REFERENCES public."location" (unlocode),
-    CONSTRAINT leg_unload_location_fk FOREIGN KEY (unload_location) REFERENCES public."location" (unlocode),
-    CONSTRAINT leg_cargo_fk FOREIGN KEY (cargo_tracking_id) REFERENCES public.cargo (tracking_id)
+    CONSTRAINT leg_unload_location_fk FOREIGN KEY (unload_location) REFERENCES public."location" (unlocode)
 );
