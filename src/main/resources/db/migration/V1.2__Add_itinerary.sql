af109304e5137ee5488542ee61a95f566b9bfd43
gushakov <Georgy.Ushakov@unil.ch>
2022-07-07 13:43:15 +0200

    add leg table

---
+++ src/main/resources/db/migration/V1.2__Add_itinerary.sql
@@ -0,0 +1,12 @@
+CREATE TABLE public.leg
+(
+    cargo_tracking_id varchar   NOT NULL,
+    load_location     varchar   NOT NULL,
+    unload_location   varchar   NOT NULL,
+    load_time         timestamp NOT NULL,
+    unload_time       timestamp NOT NULL,
+    leg_index         int       NOT NULL,
+    CONSTRAINT leg_load_location_fk FOREIGN KEY (load_location) REFERENCES public."location" (unlocode),
+    CONSTRAINT leg_unload_location_fk FOREIGN KEY (unload_location) REFERENCES public."location" (unlocode),
+    CONSTRAINT leg_cargo_fk FOREIGN KEY (cargo_tracking_id) REFERENCES public.cargo (tracking_id)
+);
