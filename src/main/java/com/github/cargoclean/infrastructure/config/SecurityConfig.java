4b875a19196baf5d41671e342ce3836bc63064ca
gushakov <Georgy.Ushakov@unil.ch>
2024-06-19 18:04:44 +0200

    added, JaCoCo plugin, encrypted passwords

--- src/main/java/com/github/cargoclean/infrastructure/config/SecurityConfig.java
+++ src/main/java/com/github/cargoclean/infrastructure/config/SecurityConfig.java
@@ -18,10 +18,12 @@ import static org.springframework.security.config.Customizer.withDefaults;
     References:
     ----------
 
-    1. WebSecurityCustomizer, JavaDocs
+    1. WebSecurityCustomizer, JavaDoc
     2. Spring Security Configuration, Java: https://docs.spring.io/spring-security/reference/servlet/configuration/java.html
     3. Spring Security, override HttpSecurity example: https://github.com/spring-projects/spring-security-samples/blob/main/servlet/java-configuration/max-sessions/src/main/java/example/SecurityConfiguration.java
     4. Spring Security Documentation, Multiple security configurations: https://docs.spring.io/spring-security/reference/servlet/configuration/java.html#_multiple_httpsecurity
+    5. Spring, password encoders: https://mkyong.com/spring-boot/spring-security-there-is-no-passwordencoder-mapped-for-the-id-null/
+    6. Spring, "org.springframework.security.core.userdetails.User.withDefaultPasswordEncoder", JavaDoc
  */
 
 
@@ -87,16 +89,14 @@ public class SecurityConfig {
         InMemoryUserDetailsManager userDetailsManager = new InMemoryUserDetailsManager();
 
         userDetailsManager.createUser(
-                User.withDefaultPasswordEncoder()
-                        .username("agent")
-                        .password("test")
+                User.withUsername("agent")
+                        .password("{bcrypt}$2a$10$Zxwpz/RLcEo.S/PkO99gHOTbRaI/m3SZtfWzKYvRavFmD6XRDDjwi") // "test" (encrypted)
                         .authorities(ROLE_CARGO_AGENT)
                         .build());
 
         userDetailsManager.createUser(
-                User.withDefaultPasswordEncoder()
-                        .username("manager")
-                        .password("test")
+                User.withUsername("manager")
+                        .password("{bcrypt}$2a$10$XNr3hwkgDpNPEFtQl4q6E.xS92OEwiOd3tx0/4ovNMkw1mIXeoAW6") // "test" (encrypted)
                         .authorities(ROLE_CARGO_AGENT, ROLE_CARGO_MANAGER)
                         .build());
 
